d97e79b21126da967ec37f76118479cc03327dc8
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-05 17:57:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key] : value})
+}
+ function destructivelyUpdateObjectWithKeyAndValue( obj, key, value) { obj [key] = value
+ return obj
+}
+function deleteFromObjectByKey(obj, key) { delete obj[key]
+return obj
+}
+function deleteFromObjectByKey(obj, key) {
+  var clone = Object.assign({}, obj)
+  delete clone[key]
+  return clone
+}
